3156de0ecdf835dd658a3beda4d71618d5d762ef
Ibai Irastorza <ibailasalle@gmail.com>
2020-04-30 09:59:05 +0200

    Desesperación

--- SpotyRun/app/src/main/java/com/example/spotyrun/MainActivity.java
+++ SpotyRun/app/src/main/java/com/example/spotyrun/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.spotyrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.PlayerApi;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.CallResult;
+import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
@@ -59,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void connected() {
         // Play a playlist
-        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:6w6jcgXEv3RrjTHLxmcI6F");
 
         // Subscribe to PlayerState
         mSpotifyAppRemote.getPlayerApi()
@@ -69,6 +91,9 @@ public class MainActivity extends AppCompatActivity {
                     if (track != null) {
                         Log.d("MainActivity", track.name + " by " + track.artist.name);
                     }
+                    Log.d("COMPROBACION", String.valueOf(track.album).contains("Aladdín")+"");
                 });
+
     }
+
 }
